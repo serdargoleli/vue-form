b677080b900f094799ff96c768e64653c7064a82
serdargoleli <web.serdargoleli@gmail.com>
2022-12-15 01:06:21 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <section class="flex items-center justify-center bg-gray-100">
-      <div class="main-content-form p-8 w-4/12 h-full">
-        <a href="" class="mb-8 inline-block">
-          <logo />
-        </a>
+    <main class="main">
+      <div class="main-content-form">
+        <h1 class="title-1">Contact Us!</h1>
+        <small class="subtitle">Contact us for your inquiries.</small>
 
-        <h1 class="mb-2 text-xl font-bold tracking-tight leading-none text-neutral-800 text-2xl">Bizimle İletişime Geçin!</h1>
-        <small class="text-gray-500 mb-5 inline-block">Merak ettikleriniz için bizimle iletişime geçin.</small>
-
-        <form class=" " action="">
+        <form class="form" action="">
           <!-- FULLNAME -->
           <div class="mb-4">
-            <label for="fullname" class="block mb-1 text-sm font-medium text-gray-600">Fullname</label>
+            <label for="fullname">Fullname</label>
             <input
               v-model="fullname"
               type="text"
               id="fullname"
-              class="bg-gray-200 border border-gray-300 placeholder:text-gray-400 text-gray-700 text-sm rounded-lg focus:ring-gray-600 focus:border-gray-400 block w-full p-2.5"
+              class="form-input placeholder:text-gray-400"
               placeholder="john DOE"
               tabindex="1"
               required
@@ -26,21 +22,22 @@
 
           <!-- EMAIL -->
           <div class="mb-4">
-            <label for="email" class="block mb-1 text-sm font-medium text-gray-600">E-Mail</label>
+            <label for="email">E-Mail</label>
             <input
               v-model="email"
               type="email"
               id="email"
-              class="bg-gray-200 border border-gray-300 placeholder:text-gray-400 text-gray-700 text-sm rounded-lg focus:ring-gray-800 focus:border-gray-400 block w-full p-2.5"
+              class="form-input placeholder:text-gray-400"
               placeholder="johndoe@info.com"
               @blur="validEmail"
+              @input="validEmail"
             />
             <small class="font-xs text-red-400">{{ emailInvalidMsg }}</small>
           </div>
 
           <!-- PHONE -->
           <div class="mb-4">
-            <label for="phone" class="block mb-1 text-sm font-medium text-gray-600">Phone</label>
+            <label for="phone">Phone</label>
             <vue-tel-input
               class="bg-gray-200 rounded-lg text-gray-700 text-sm"
               :autoFormat="true"
@@ -55,18 +52,17 @@
 
           <!-- ADDRESS -->
           <div class="mb-4">
-            <label for="address" class="block mb-1 text-sm font-medium text-gray-600">Address</label>
+            <label for="address">Address</label>
             <textarea
               id="address"
               rows="3"
-              class="bg-gray-200 border border-gray-300 placeholder:text-gray-400 text-gray-700 text-sm rounded-lg focus:ring-gray-800 focus:border-gray-400 block w-full p-2.5"
+              class="form-input placeholder:text-gray-400"
               placeholder="Write your thoughts here..."
               v-model="address"
             ></textarea>
           </div>
           <button
             type="submit"
-            class="w-full text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
             :class="formControl ? 'cursor-pointer bg-blue-700 hover:bg-blue-900' : 'cursor-not-allowed bg-blue-400'"
             :disabled="!formControl"
           >
@@ -74,25 +70,19 @@
           </button>
         </form>
       </div>
-    </section>
+    </main>
   </div>
 </template>
 
 <script>
-  import logo from "./components/logo.vue";
-
   export default {
     name: "App",
-
-    components: {
-      logo,
-    },
     data() {
       return {
         emailInvalidMsg: "",
         phoneInvalidMsg: "",
-        fullname: "asdasd",
-        email: "asdasd@gmail.com",
+        fullname: "",
+        email: "",
         phone: {
           number: "",
           valid: true,
@@ -107,8 +97,7 @@
           width: "390px",
         },
         inputOptions: {
-          styleClasses:
-            "bg-gray-200 border border-gray-300 placeholder:text-gray-400 text-gray-700  rounded-lg focus:ring-gray-800 focus:border-gray-400 block  p-2.5",
+          styleClasses: "form-input",
           required: true,
           type: "tel",
           mod: "national",
@@ -150,10 +139,6 @@
         } else {
           this.emailInvalidMsg = "";
         }
-      },
-      typing(event) {
-        this.address = event.target.value;
-        console.log(this.address);
       },
     },
   };
